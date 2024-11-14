f968140a5e281ef3dfdc2a6d68e849964bf4f878
An Phung <tphung01@student.ubc.ca>
2024-11-13 20:03:59 -0800

    init PetsScreen

--- src/main/ui/GUI/Screens/AppScreens/PetsScreen.java
+++ src/main/ui/GUI/Screens/AppScreens/PetsScreen.java
@@ -2,16 +2,38 @@ package ui.GUI.Screens.AppScreens;
 
 import java.awt.GridLayout;
 
-import javax.swing.JPanel;
+import javax.swing.*;
+import javax.swing.border.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import ui.GUI.MainApp;
 import ui.GUI.Screens.AppScreen;
 
 public class PetsScreen extends AppScreen {
+    private MainApp mainApp;
 
     // Screen to choose Pet Game
     public PetsScreen(MainApp app) {
-        super(app, "Pet Picking");
+        super(app, "House");
+        this.mainApp = super.app;
+
+        addScrollPane();
+        createMenuButton();
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adding a scroll pane with Pet Button to go to Pet Game
+    private void addScrollPane() {
+        // stub
+    }
+
+    // MODIFIES: this
+    // EFFECTS: adding Pets button for each pet in house
+    //          open Pet Game when clicked
+    private void addButton(JPanel jPanel) {
+        // stub
     }
 
 }
