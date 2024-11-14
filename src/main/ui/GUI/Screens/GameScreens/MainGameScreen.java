2f6885766fdf9373a84050229c5645cd70250a6e
An Phung <tphung01@student.ubc.ca>
2024-11-14 01:23:17 -0800

    handle Image display

--- src/main/ui/GUI/Screens/GameScreens/MainGameScreen.java
+++ src/main/ui/GUI/Screens/GameScreens/MainGameScreen.java
@@ -12,10 +12,40 @@ public class MainGameScreen extends GameScreen {
     public MainGameScreen(PetGameApp petGameApp) {
         super(petGameApp, "Pet Room");
 
-        JLabel petLabel = new JLabel(app.getPet().getName());
+        ImageIcon imageIcon = new ImageIcon(handleImagePath()); // Replace with your image
+                                                                                          // path
+        Image image = imageIcon.getImage();
+        Image scaledImage = image.getScaledInstance(480, 720, Image.SCALE_SMOOTH);
+
+        JLabel petLabel = new JLabel(new ImageIcon(scaledImage));
         add(petLabel, BorderLayout.CENTER);
 
         createMenuButton();
+    }
+
+
+    // EFFECTS: return the correct image based on the Pet's Status
+    private String handleImagePath() {
+        String source = "src\\main\\ui\\GUI\\PetImage\\";
+        String petType = pet.getType();
+        String petStatus = "";
+        if (pet.needsPill()) {
+            petStatus = "Injuried";
+        } else if (pet.needsAttention()[0] && pet.needsAttention()[1]) {
+            petStatus = "HungryDirty";
+        } else if (pet.needsAttention()[0] ) {
+            petStatus = "Hungry";
+        } else if (pet.needsAttention()[1]) {
+            petStatus = "Dirty";
+        } else if (pet.needsAttention()[2]) {
+            petStatus = "Sad";
+        }
+
+        if (petStatus == "") {
+            return source + petType + ".png";
+        } else {
+            return source + petStatus + "\\" + petType + ".png";
+        }
     }
 
 }
