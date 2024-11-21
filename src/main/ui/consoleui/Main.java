76e29a136c7a9d4a1007dc6339d8cb8f5bc8a53e
An Phung <tphung01@student.ubc.ca>
2024-11-20 21:11:36 -0800

    Rename GUI and ConsoleUI file

--- src/main/ui/consoleui/Main.java
+++
@@ -1,9 +0,0 @@
-package ui.consoleui;
-
-
-// public class Main {
-//     public static void main(String[] args) {
-//         GameUI newGame = new GameUI();
-//         newGame.startGame();
-//     }
-// }
